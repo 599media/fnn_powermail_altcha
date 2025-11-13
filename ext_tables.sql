c3b7fdc657ac76cd5fc516bf2d633419e1cde613
Erik Konrad <erik.konrad@599media.de>
2025-11-13 09:45:34 +0100

    Github Issue #2 - DB field size fix
    reduces the field size for field "challenge" to 256 Byte to avoid compatibiliy issues with field index

--- ext_tables.sql
+++ ext_tables.sql
@@ -5,7 +5,7 @@
 CREATE TABLE tx_fnnpowermailaltcha_domain_model_challenge (
 	uid int(11) NOT NULL auto_increment,
 	pid int(11) DEFAULT '0' NOT NULL,
-	challenge VARCHAR(1024) DEFAULT '' NOT NULL,
+	challenge VARCHAR(256) DEFAULT '' NOT NULL,
 	expires int(11) unsigned DEFAULT '0' NOT NULL,
 	solved tinyint(4) DEFAULT '0' NOT NULL,
 
